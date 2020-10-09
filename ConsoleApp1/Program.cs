3ede0f4880a1cd1f4b24dddc70a7a4c344b37562
Lazerneek123 <Lazerneek123@yandex.ru>
2020-10-09 13:24:53 +0300

    Triangle

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть сторони трикутника:");
+
+            TTriangle triangle = new TTriangle();
+            int a = triangle.A;
+            int b = triangle.B;
+            int c = triangle.C;
+
+            Console.WriteLine("Сторони вашого трикутника: {0}, {1}, {2}.", a, b, c);
+          
+            int check = triangle.Check_The_Triangle(a, b, c);
+
+            if (check != 1)
+            {
+                double P = triangle.Perimeter_Triangle(a, b, c);
+                double S = triangle.Area_Triangle(P);
+
+                Console.WriteLine("Периметр даного трикутника = {0}, площа = {1}.", P, S);
+            }
+
+            Console.ReadKey();
+
+        }        
+
+    }
+}
---
+++ ConsoleApp1/TTriangle.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TTriangle
+    {
+        protected int a = GetSide(0);
+        protected int b = GetSide(0);
+        protected int c = GetSide(0);
+
+        static int GetSide(int side)
+        {
+            side = Int32.Parse(Console.ReadLine());
+            return side;
+        }
+
+
+        public int Check_The_Triangle(int a, int b, int c)
+        {
+            if (a + b < c || a + c < b || b + c < a)
+            {
+                Console.WriteLine("Такий трикутник не існує!");
+                a = 1;
+            }
+            else
+            {
+                a = 0;
+            }
+
+            return a;
+        }
+
+        public double Perimeter_Triangle(int a, int b, int c)
+        {
+            double P = a + b + c;
+
+            return P;
+        }
+
+
+        public double Area_Triangle(double P)
+        {
+            double p = P / 2;
+
+            double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            return S;
+        }
+
+
+        public int A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    a = value;
+                }
+                
+            }
+        }
+
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    b = value;
+                }
+            }
+        }
+
+        public int C
+        {
+            get
+            {
+                return c;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    c = value;
+                }
+            }
+        }
+
+    }
+}
